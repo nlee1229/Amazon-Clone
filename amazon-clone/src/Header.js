844b0b502455172f2b7d968a90c093a6a2845b7f
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-08 21:56:27 -0800

    header bar

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -4,7 +4,23 @@ import './Header.css';
 function Header() {
     return (
         <div className="header">
-            
+            <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+
+            <div className="header_search">
+                <input className="header_searchInput" type="text" />
+            </div>
+
+            <div className="header_nav">
+                <div className="header_option">
+
+                </div>
+                <div className="header_option">
+                    
+                </div>
+                <div className="header_option">
+                    
+                </div>
+            </div>
         </div>
     );
 }
