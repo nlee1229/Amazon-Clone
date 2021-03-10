49ab3dc5acb6ed5d09f0f673fd4c79856b22b1b8
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-10 14:44:47 -0800

    minor changes

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -6,9 +6,21 @@ function Home() {
         <div classNam="home">
             <div className="home_container">
                 <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" />
+                
+                <div className="home_row">
+
+                </div>
+
+                <div className="home_row">
+                    
+                </div>
+
+                <div className="home_row">
+                    
+                </div>
             </div>
         </div>
     );
-};
+}
 
 export default Home;
