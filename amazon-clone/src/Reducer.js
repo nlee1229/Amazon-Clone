241b7562b0132807b083333b0e403c91678a5cbf
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-10 21:16:30 -0800

    added in subtotal

--- amazon-clone/src/Reducer.js
+++ amazon-clone/src/Reducer.js
@@ -2,6 +2,10 @@ export const initialState = {
     cart: [],
 };
 
+// Selector
+export const getCartTotal = (cart) => 
+    cart?.reduce((amount, item) => item.price + amount, 0); // Reduce iterates through the cart and tallies the total... Have an initial amount, then add to the total
+
 const reducer = (state, action) => {
     console.log(action);
     switch(action.type) {
