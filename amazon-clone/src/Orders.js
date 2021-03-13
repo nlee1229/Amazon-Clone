89c8d34edc1991158d59e3c3c64ac64471ab8610
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-13 13:37:23 -0800

    adding orders page

--- amazon-clone/src/Orders.js
+++ amazon-clone/src/Orders.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { db } from "./firebase";
 import "./Orders.css";
+import { useStateValue } from "./StateProvider";
 
 function Orders() {
+    const [{ cart, user }, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    // when the orders component loads, this useEffect runs
+    useEffect(() => {
+        db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot(snapshot => { // gives us a real time snapshot of the db
+
+        })
+    }, [])
+
     return (
         <div className="orders">
             <h1>Your Orders</h1>
-            
+
         </div>
     );
 }
