e02b53e0996cf8476312a56473299fcf6b866af2
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-12 16:59:43 -0800

    minor changes

---
+++ amazon-clone/functions/index.js
@@ -0,0 +1,15 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51IUKWwJpaSoRz1wyaPPiKBoozEBCUXfxnSVMdUndukO6NLP3U2WbvhRBVHHrJj54RrnDMu7tc5nq0dEIhmPSuo9J00q29hr02v');
+
+
+// ALL BELOW IS TO SET UP AN API
+
+// App config
+const app = express();
+// Middlewares
+app.use(cors({origin: true}));
+// API routes
+
+// Listen command
--- amazon-clone/src/Payment.js
+++ amazon-clone/src/Payment.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CheckoutProduct from './CheckoutProduct';
 import "./Payment.css";
 import {useStateValue} from "./StateProvider";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getCartTotal } from "./Reducer";
+import axios from "./axios";
 
 function Payment() {
     const [{cart, user}, dispatch] = useStateValue();
+    const history = useHistory();
 
     const stripe = useStripe();
     const elements = useElements();
 
-    const handleSubmit = e => {
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
+
+    useEffect(() => {
+        // generate the special stripe secret which allows us to charge a user
+        // ** everything in this function ** whenever the cart changes, we need a new secret b/c the price changes. It will update the special dtripe swcret which allows us to charge the correct amount
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: 'post',
+                // Stripe expects the total in a currencies subunits (if you're using dollars, it expects you to pass a total amount in cents, but to prevent that we do this )
+                url: `/payments/create?total=${getCartTotal(cart) * 100}`
+            });
+            setClientSecret(response.data.clientSecret)
+        }
+        getClientSecret();
+    }, [cart])
+
+    const handleSubmit = async (event) => {
         // all stripe things
+        event.preventDefault(); // will stop it from refreshing
+        setProcessing(true); // set processing to true... stop users from clicking the button multiple times
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement)
+            }
+        }).then(({ paymentIntent }) => {
+            // paymentIntent is basically payment confirmation
+            setSucceeded(true);
+            setError(null);
+            setProcessing(false);
+
+            history.replace("/orders") // after placing an order, we will send users to the order page
+        })
+    }
+
+    const handleChange = e => {
+        // listens for changes in CardElement & display any errors as user types their card details
+        setDisabled(e.empty); // if event is empty, then disable the button
+        setError(e.error ? e.error.message: ""); // if there is an error, show the error or show nothing
     }
 
     return (
@@ -59,7 +104,26 @@ function Payment() {
                     </div>
                     <div className="payment_details">
                         <form onSubmit={handleSubmit}>
-                            <CardElement />
+                            <CardElement onChange={handleChange} />
+
+                            <div className="payment_priceContainer">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>Order Total: {value}</h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getCartTotal(cart)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled || succeeded}>
+                                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                                </button>
+                            </div>
+
+                            {/* Errors */}
+                            {error && <div>{error}</div>}
                         </form>
                         
                     </div>
